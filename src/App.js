42017f51db608ab686dff8296dde752257651cd4
vivek <vivekkumarvky13@gmail.com>
2022-05-12 18:11:01 +0530

    master

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="/fav" element={<Fav />}></Route>
+     
       </Routes>
     </div>
   );
